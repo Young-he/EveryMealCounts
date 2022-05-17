b20215d8e3b45ca6250e049d7617b12f46c3dd0c
Sail <younghe@outlookcom>
2022-05-17 16:43:00 +0800

    Dada

--- src/main/java/priv/younghe/everymealcounts/HelloApplication.java
+++ src/main/java/priv/younghe/everymealcounts/HelloApplication.java
@@ -6,5 +6,17 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/api")
 public class HelloApplication extends Application
 {
+    public static void main(String[] args)
+    {
+        System.out.println("kfdjsaklfjlkads");
+        System.out.println("kfdjsaklfjlkads");
+        System.out.println("kfdjsaklfjlkads");
+        System.out.println("kfdjsaklfjlkads");
+        System.out.println("kfdjsaklfjlkads");
+        System.out.println("kfdjsaklfjlkads");
+        System.out.println("kfdjsaklfjlkads");
+        System.out.println("kfdjsaklfjlkads");
+        System.out.println("kfdjsaklfjlkads");
+    }
 
 }
