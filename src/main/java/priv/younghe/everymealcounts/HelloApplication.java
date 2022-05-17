17b40c4596e8882a92efd86663efa23815b421ac
Sail <younghe@outlookcom>
2022-05-17 16:41:40 +0800

    Dada

---
+++ src/main/java/priv/younghe/everymealcounts/HelloApplication.java
@@ -0,0 +1,10 @@
+package priv.younghe.everymealcounts;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application
+{
+
+}
